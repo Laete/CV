5c8adc8217aa60fe341e228416f20bb75ee3a3ff
Anna Karpelevich <anna@gdm.su>
2012-08-05 22:34:40 +0400

    basic commit: basic functions
    The most important functionality. Working on design now

---
+++ js/cv.js
@@ -0,0 +1,31 @@
+$(function() {
+	$(".accordion").accordion(
+	{
+		autoHeight: false,
+		active: false,
+		collapsible: true,
+		header: '>h4'
+	});
+});
+
+$(document).on("click", ".question", function(event){
+	var id = $(event.target).attr("id").replace("question", "answer");
+	$(this).parents(".content").find(".answer").hide();
+	$(this).parents(".content").find("#"+id).show();
+});
+
+$(document).on("click", ".tab-item > a", function(event){
+	event.preventDefault();	
+	$(".tab-item").removeClass("active");
+	$(this).parent().addClass("active");
+	
+	var id = "#"+$(this).attr("id").replace("tab-", "");
+	$(".content").hide();
+	$(id).show();
+	$(id).find(".accordion").accordion({
+		autoHeight: false,
+		active: false,
+		collapsible: true,
+		header: '>h4'
+	});
+});
