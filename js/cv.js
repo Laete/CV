a3a6d3008e54c3dd95f154e17dc5076b05a57dc6
Anna Karpelevich <anna@gdm.su>
2012-08-06 01:28:44 +0400

    design nearlly finished
    green colors, headers, answers in the third tab

--- js/cv.js
+++ js/cv.js
@@ -12,6 +12,13 @@ $(document).on("click", ".question", function(event){
 	var id = $(event.target).attr("id").replace("question", "answer");
 	$(this).parents(".content").find(".answer").hide();
 	$(this).parents(".content").find("#"+id).show();
+	var height = $("#general").height();
+	$(".answer").height(height - 20);
+});
+
+$(window).resize(function(){
+	var height = $("#general").height();
+	$(".answer").height(height - 20);
 });
 
 $(document).on("click", ".tab-item > a", function(event){
